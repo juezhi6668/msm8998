832dd7fc28321fd0febc869eb8e71ea096b83692
Al Viro <viro@zeniv.linux.org.uk>
2016-04-27 13:25:33 -0400

    atomic_open(): don't bother with EEXIST check - it's done in do_last()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/namei.c
+++ fs/namei.c
@@ -3110,11 +3110,6 @@ static int atomic_open(struct nameidata *nd, struct dentry *dentry,
 				error = create_error;
 				goto out;
 			}
-		} else {
-			if (excl && !(*opened & FILE_CREATED)) {
-				error = -EEXIST;
-				goto out;
-			}
 		}
 		goto looked_up;
 	}
