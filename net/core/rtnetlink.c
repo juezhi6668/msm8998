34593fbe6d017795a1d5e2f12b3aba83515c7dd0
Brenden Blanco <bblanco@plumgrid.com>
2016-07-20 17:22:34 -0700

    rtnl: protect do_setlink from IFLA_XDP_ATTACHED
    The IFLA_XDP_ATTACHED nested attribute is meant for read-only, and while
    do_setlink properly ignores it, it should be more paranoid and reject
    commands that try to set it.
    
    Signed-off-by: Brenden Blanco <bblanco@plumgrid.com>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -2095,6 +2095,10 @@ static int do_setlink(const struct sk_buff *skb,
 		if (err < 0)
 			goto errout;
 
+		if (xdp[IFLA_XDP_ATTACHED]) {
+			err = -EINVAL;
+			goto errout;
+		}
 		if (xdp[IFLA_XDP_FD]) {
 			err = dev_change_xdp_fd(dev,
 						nla_get_s32(xdp[IFLA_XDP_FD]));
