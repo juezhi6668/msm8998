8880533b31d788be3342ae2d899c667975762f27
Ido Schimmel <idosch@mellanox.com>
2016-12-03 16:45:06 +0100

    UPSTREAM: ipv4: fib: Allow for consistent FIB dumping
    The next patch will enable listeners of the FIB notification chain to
    request a dump of the FIB tables. However, since RTNL isn't taken during
    the dump, it's possible for the FIB tables to change mid-dump, which
    will result in inconsistency between the listener's table and the
    kernel's.
    
    Allow listeners to know about changes that occurred mid-dump, by adding
    a change sequence counter to each net namespace. The counter is
    incremented just before a notification is sent in the FIB chain.
    
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/netns/ipv4.h
+++ include/net/netns/ipv4.h
@@ -109,6 +109,9 @@ struct netns_ipv4 {
 	struct fib_rules_ops	*mr_rules_ops;
 #endif
 #endif
+
+	unsigned int	fib_seq;	/* protected by rtnl_mutex */
+
 	atomic_t	rt_genid;
 	siphash_key_t	ip_id_key;
 };
