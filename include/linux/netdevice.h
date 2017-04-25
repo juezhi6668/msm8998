cb0c21c7048332ab8d67774dedc350d6bf2f78c6
Eric Dumazet <edumazet@google.com>
2017-04-25 11:36:52 -0700

    UPSTREAM: net: move xdp_prog field in RX cache lines
    (struct net_device, xdp_prog) field should be moved in RX cache lines,
    reducing latencies when a single packet is received on idle host,
    since netif_elide_gro() needs it.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1773,6 +1773,7 @@ struct net_device {
 
 #endif
 
+	struct bpf_prog __rcu	*xdp_prog;
 	unsigned long		gro_flush_timeout;
 	rx_handler_func_t __rcu	*rx_handler;
 	void __rcu		*rx_handler_data;
@@ -1889,7 +1890,6 @@ struct net_device {
 	struct lock_class_key	*qdisc_tx_busylock;
 	struct lock_class_key	*qdisc_running_key;
 	bool			proto_down;
-	struct bpf_prog __rcu	*xdp_prog;
 };
 #define to_net_dev(d) container_of(d, struct net_device, dev)
 
