a3f31edfae1ff8db8b62d96c9b25e753f5a1d1f0
Eric Dumazet <edumazet@google.com>
2017-07-03 02:57:54 -0700

    UPSTREAM: net: make sk_ehashfn() static
    sk_ehashfn() is only used from a single file.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/inet_hashtables.h
+++ include/net/inet_hashtables.h
@@ -351,7 +351,6 @@ static inline struct sock *__inet_lookup_skb(struct inet_hashinfo *hashinfo,
 				     iph->daddr, dport, inet_iif(skb));
 }
 
-u32 sk_ehashfn(const struct sock *sk);
 u32 inet6_ehashfn(const struct net *net,
 		  const struct in6_addr *laddr, const u16 lport,
 		  const struct in6_addr *faddr, const __be16 fport);
