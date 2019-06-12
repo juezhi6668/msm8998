9f5e4922147d121abcd0805117c81393915005ca
Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
2019-06-13 00:21:40 +0530

    powerpc/bpf: use unsigned division instruction for 64-bit operations
    commit 758f2046ea040773ae8ea7f72dd3bbd8fa984501 upstream.
    
    BPF_ALU64 div/mod operations are currently using signed division, unlike
    BPF_ALU32 operations. Fix the same. DIV64 and MOD64 overflow tests pass
    with this fix.
    
    Fixes: 156d0e290e969c ("powerpc/ebpf/jit: Implement JIT compiler for extended BPF")
    Cc: stable@vger.kernel.org # v4.8+
    Signed-off-by: Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/powerpc/net/bpf_jit_comp64.c
+++ arch/powerpc/net/bpf_jit_comp64.c
@@ -623,19 +623,27 @@ static int bpf_jit_build_body(struct bpf_prog *fp, u32 *image,
 			PPC_JMP(exit_addr);
 			if (BPF_OP(code) == BPF_MOD) {
 <<<<<<< HEAD
+<<<<<<< HEAD
 				PPC_DIVDU(b2p[TMP_REG_1], dst_reg, src_reg);
 =======
 				PPC_DIVD(b2p[TMP_REG_1], dst_reg, src_reg);
 >>>>>>> 156d0e290e96... powerpc/ebpf/jit: Implement JIT compiler for extended BPF
+=======
+				PPC_DIVDU(b2p[TMP_REG_1], dst_reg, src_reg);
+>>>>>>> e90a7ecde5f3... powerpc/bpf: use unsigned division instruction for 64-bit operations
 				PPC_MULD(b2p[TMP_REG_1], src_reg,
 						b2p[TMP_REG_1]);
 				PPC_SUB(dst_reg, dst_reg, b2p[TMP_REG_1]);
 			} else
 <<<<<<< HEAD
+<<<<<<< HEAD
 				PPC_DIVDU(dst_reg, dst_reg, src_reg);
 =======
 				PPC_DIVD(dst_reg, dst_reg, src_reg);
 >>>>>>> 156d0e290e96... powerpc/ebpf/jit: Implement JIT compiler for extended BPF
+=======
+				PPC_DIVDU(dst_reg, dst_reg, src_reg);
+>>>>>>> e90a7ecde5f3... powerpc/bpf: use unsigned division instruction for 64-bit operations
 			break;
 		case BPF_ALU | BPF_MOD | BPF_K: /* (u32) dst %= (u32) imm */
 		case BPF_ALU | BPF_DIV | BPF_K: /* (u32) dst /= (u32) imm */
@@ -675,10 +683,14 @@ static int bpf_jit_build_body(struct bpf_prog *fp, u32 *image,
 			case BPF_ALU64:
 				if (BPF_OP(code) == BPF_MOD) {
 <<<<<<< HEAD
+<<<<<<< HEAD
 					PPC_DIVDU(b2p[TMP_REG_2], dst_reg,
 =======
 					PPC_DIVD(b2p[TMP_REG_2], dst_reg,
 >>>>>>> 156d0e290e96... powerpc/ebpf/jit: Implement JIT compiler for extended BPF
+=======
+					PPC_DIVDU(b2p[TMP_REG_2], dst_reg,
+>>>>>>> e90a7ecde5f3... powerpc/bpf: use unsigned division instruction for 64-bit operations
 							b2p[TMP_REG_1]);
 					PPC_MULD(b2p[TMP_REG_1],
 							b2p[TMP_REG_1],
@@ -687,10 +699,14 @@ static int bpf_jit_build_body(struct bpf_prog *fp, u32 *image,
 							b2p[TMP_REG_1]);
 				} else
 <<<<<<< HEAD
+<<<<<<< HEAD
 					PPC_DIVDU(dst_reg, dst_reg,
 =======
 					PPC_DIVD(dst_reg, dst_reg,
 >>>>>>> 156d0e290e96... powerpc/ebpf/jit: Implement JIT compiler for extended BPF
+=======
+					PPC_DIVDU(dst_reg, dst_reg,
+>>>>>>> e90a7ecde5f3... powerpc/bpf: use unsigned division instruction for 64-bit operations
 							b2p[TMP_REG_1]);
 				break;
 			}
