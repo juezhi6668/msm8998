583695352ccc31eeac9527e28d3c3d1e1b56fbcb
Huexxx <huexxx@gmail.com>
2023-04-14 10:00:29 +0200

    dax: soft drop DAX since we don't use it
    Change-Id: I6c7712a9bc30b1e65fe5a500e7773bdc3474db91

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -1692,6 +1692,8 @@ static int handle_mount_opt(struct super_block *sb, char *opt, int token,
 		sbi->s_jquota_fmt = m->mount_opt;
 #endif
 	} else if (token == Opt_dax) {
+
+#if 0
 #ifdef CONFIG_FS_DAX
 		ext4_msg(sb, KERN_WARNING,
 		"DAX enabled. Warning: EXPERIMENTAL, use at your own risk");
@@ -1700,6 +1702,10 @@ static int handle_mount_opt(struct super_block *sb, char *opt, int token,
 		ext4_msg(sb, KERN_INFO, "dax option not supported");
 		return -1;
 #endif
+#endif
+
+		ext4_msg(sb, KERN_INFO, "dax option not supported");
+		return -1;
 	} else {
 		if (!args->from)
 			arg = 1;
