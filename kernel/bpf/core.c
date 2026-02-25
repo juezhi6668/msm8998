e340f7529366b8c02c9018b5b888acb5cb4ba8f4
jsdizkcksv <3531236823@qq.com>
2026-02-25 21:07:40 +0800

    Revert "bpf: add __weak hook for allocating executable memory"
    This reverts commit c1addd49297e20a95b6effbd566fab90d6990192.

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -390,16 +390,6 @@ bool __weak arch_bpf_jit_check_func(const struct bpf_prog *prog)
 EXPORT_SYMBOL(arch_bpf_jit_check_func);
 #endif
 
-void *__weak bpf_jit_alloc_exec(unsigned long size)
-{
-	return module_alloc(size);
-}
-
-void __weak bpf_jit_free_exec(void *addr)
-{
-	module_memfree(addr);
-}
-
 struct bpf_binary_header *
 bpf_jit_binary_alloc(unsigned int proglen, u8 **image_ptr,
 		     unsigned int alignment,
@@ -417,8 +407,8 @@ bpf_jit_binary_alloc(unsigned int proglen, u8 **image_ptr,
 
 	if (bpf_jit_charge_modmem(pages))
 		return NULL;
-	hdr = bpf_jit_alloc_exec(size);
-		if (!hdr) {
+	hdr = module_alloc(size);
+	if (!hdr) {
 		bpf_jit_uncharge_modmem(pages);
 		return NULL;
 	}
@@ -442,7 +432,7 @@ void bpf_jit_binary_free(struct bpf_binary_header *hdr)
 {
 	u32 pages = hdr->pages;
 
-	bpf_jit_free_exec(hdr);
+	module_memfree(hdr);
 	bpf_jit_uncharge_modmem(pages);
 }
 
