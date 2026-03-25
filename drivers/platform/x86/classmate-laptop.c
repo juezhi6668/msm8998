38d79c8795b637416c2f41c85417d371ff5eec08
juezhi <1649417471@qq.com>
2026-03-25 22:14:58 +0800

    Revert "update"

--- drivers/platform/x86/classmate-laptop.c
+++ drivers/platform/x86/classmate-laptop.c
@@ -221,12 +221,7 @@ static ssize_t cmpc_accel_sensitivity_show_v4(struct device *dev,
 
 	acpi = to_acpi_device(dev);
 	inputdev = dev_get_drvdata(&acpi->dev);
-	if (!inputdev)
-		return -ENXIO;
-
 	accel = dev_get_drvdata(&inputdev->dev);
-	if (!accel)
-		return -ENXIO;
 
 	return sprintf(buf, "%d\n", accel->sensitivity);
 }
@@ -243,12 +238,7 @@ static ssize_t cmpc_accel_sensitivity_store_v4(struct device *dev,
 
 	acpi = to_acpi_device(dev);
 	inputdev = dev_get_drvdata(&acpi->dev);
-	if (!inputdev)
-		return -ENXIO;
-
 	accel = dev_get_drvdata(&inputdev->dev);
-	if (!accel)
-		return -ENXIO;
 
 	r = kstrtoul(buf, 0, &sensitivity);
 	if (r)
@@ -280,12 +270,7 @@ static ssize_t cmpc_accel_g_select_show_v4(struct device *dev,
 
 	acpi = to_acpi_device(dev);
 	inputdev = dev_get_drvdata(&acpi->dev);
-	if (!inputdev)
-		return -ENXIO;
-
 	accel = dev_get_drvdata(&inputdev->dev);
-	if (!accel)
-		return -ENXIO;
 
 	return sprintf(buf, "%d\n", accel->g_select);
 }
@@ -302,12 +287,7 @@ static ssize_t cmpc_accel_g_select_store_v4(struct device *dev,
 
 	acpi = to_acpi_device(dev);
 	inputdev = dev_get_drvdata(&acpi->dev);
-	if (!inputdev)
-		return -ENXIO;
-
 	accel = dev_get_drvdata(&inputdev->dev);
-	if (!accel)
-		return -ENXIO;
 
 	r = kstrtoul(buf, 0, &g_select);
 	if (r)
@@ -336,8 +316,6 @@ static int cmpc_accel_open_v4(struct input_dev *input)
 
 	acpi = to_acpi_device(input->dev.parent);
 	accel = dev_get_drvdata(&input->dev);
-	if (!accel)
-		return -ENXIO;
 
 	cmpc_accel_set_sensitivity_v4(acpi->handle, accel->sensitivity);
 	cmpc_accel_set_g_select_v4(acpi->handle, accel->g_select);
@@ -592,12 +570,7 @@ static ssize_t cmpc_accel_sensitivity_show(struct device *dev,
 
 	acpi = to_acpi_device(dev);
 	inputdev = dev_get_drvdata(&acpi->dev);
-	if (!inputdev)
-		return -ENXIO;
-
 	accel = dev_get_drvdata(&inputdev->dev);
-	if (!accel)
-		return -ENXIO;
 
 	return sprintf(buf, "%d\n", accel->sensitivity);
 }
@@ -614,12 +587,7 @@ static ssize_t cmpc_accel_sensitivity_store(struct device *dev,
 
 	acpi = to_acpi_device(dev);
 	inputdev = dev_get_drvdata(&acpi->dev);
-	if (!inputdev)
-		return -ENXIO;
-
 	accel = dev_get_drvdata(&inputdev->dev);
-	if (!accel)
-		return -ENXIO;
 
 	r = kstrtoul(buf, 0, &sensitivity);
 	if (r)
