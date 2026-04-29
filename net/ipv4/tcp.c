16d94b15c4e8eb44fa3fba1629af610a644eed2a
juezhi6668 <1649417471@qq.com>
2026-04-29 11:25:58 +0800

    fix

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -2961,17 +2961,17 @@ struct sk_buff *tcp_get_timestamping_opt_stats(const struct sock *sk)
 	struct sk_buff *stats;
 	struct tcp_info info;
 
-	stats = alloc_skb(3 * nla_total_size_64bit(sizeof(u64)), GFP_ATOMIC);
+	stats = alloc_skb(3 * nla_total_size(sizeof(u64)), GFP_ATOMIC);
 	if (!stats)
 		return NULL;
 
 	tcp_get_info_chrono_stats(tp, &info);
-	nla_put_u64_64bit(stats, TCP_NLA_BUSY,
-			  info.tcpi_busy_time, TCP_NLA_PAD);
-	nla_put_u64_64bit(stats, TCP_NLA_RWND_LIMITED,
-			  info.tcpi_rwnd_limited, TCP_NLA_PAD);
-	nla_put_u64_64bit(stats, TCP_NLA_SNDBUF_LIMITED,
-			  info.tcpi_sndbuf_limited, TCP_NLA_PAD);
+	nla_put_u64(stats, TCP_NLA_BUSY,
+			  info.tcpi_busy_time);
+	nla_put_u64(stats, TCP_NLA_RWND_LIMITED,
+			  info.tcpi_rwnd_limited);
+	nla_put_u64(stats, TCP_NLA_SNDBUF_LIMITED,
+			  info.tcpi_sndbuf_limited);
 	return stats;
 }
 
