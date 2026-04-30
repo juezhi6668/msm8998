e31ee5af2d9f8ee3a372785fb7b23be0eb51c58f
juezhi6668 <1649417471@qq.com>
2026-04-30 13:41:02 +0800

    Revert "net: l3mdev: Add hook in ip and ipv6"
    This reverts commit abdf1976313c7ec30201c43cad3ff708982a60de.

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -838,9 +838,7 @@ struct tcp_skb_cb {
  */
 static inline int tcp_v6_iif(const struct sk_buff *skb)
 {
-	bool l3_slave = skb_l3mdev_slave(TCP_SKB_CB(skb)->header.h6.flags);
-
-	return l3_slave ? skb->skb_iif : TCP_SKB_CB(skb)->header.h6.iif;
+	return TCP_SKB_CB(skb)->header.h6.iif;
 }
 #endif
 
--- net/core/dev.c
+++ net/core/dev.c
@@ -1891,7 +1891,7 @@ static inline bool skb_loop_sk(struct packet_type *ptype, struct sk_buff *skb)
  *	taps currently in use.
  */
 
-void dev_queue_xmit_nit(struct sk_buff *skb, struct net_device *dev)
+static void dev_queue_xmit_nit(struct sk_buff *skb, struct net_device *dev)
 {
 	struct packet_type *ptype;
 	struct sk_buff *skb2 = NULL;
@@ -1948,7 +1948,6 @@ out_unlock:
 		pt_prev->func(skb2, skb->dev, pt_prev, skb->dev);
 	rcu_read_unlock();
 }
-EXPORT_SYMBOL_GPL(dev_queue_xmit_nit);
 
 /**
  * netif_setup_tc - Handle tc mappings on real_num_tx_queues change
