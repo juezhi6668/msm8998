1f4ddf1122cc8d290d0161e0a9be73d61c9675a2
juezhi6668 <1649417471@qq.com>
2026-04-30 14:46:34 +0800

    Revert "mlxsw: spectrum: Make vFID struct generic"
    This reverts commit 2b8db5d021588ddc71290a608ceef3b35853fb8a.

--- drivers/net/ethernet/mellanox/mlxsw/spectrum.h
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum.h
@@ -87,11 +87,11 @@ struct mlxsw_sp_upper {
 	unsigned int ref_count;
 };
 
-struct mlxsw_sp_fid {
+struct mlxsw_sp_vfid {
 	struct list_head list;
-	unsigned int ref_count;
-	struct net_device *dev;
-	u16 fid;
+	u16 nr_vports;
+	u16 vfid;	/* Starting at 0 */
+	struct net_device *br_dev;
 	u16 vid;
 };
 
@@ -217,7 +217,7 @@ struct mlxsw_sp_port {
 	u16 lag_id;
 	struct {
 		struct list_head list;
-		struct mlxsw_sp_fid *f;
+		struct mlxsw_sp_vfid *vfid;
 		u16 vid;
 	} vport;
 	struct {
@@ -262,13 +262,13 @@ mlxsw_sp_port_lagged_get(struct mlxsw_sp *mlxsw_sp, u16 lag_id, u8 port_index)
 static inline bool
 mlxsw_sp_port_is_vport(const struct mlxsw_sp_port *mlxsw_sp_port)
 {
-	return mlxsw_sp_port->vport.f;
+	return mlxsw_sp_port->vport.vfid;
 }
 
 static inline struct net_device *
 mlxsw_sp_vport_br_get(const struct mlxsw_sp_port *mlxsw_sp_vport)
 {
-	return mlxsw_sp_vport->vport.f->dev;
+	return mlxsw_sp_vport->vport.vfid->br_dev;
 }
 
 static inline u16
@@ -278,9 +278,9 @@ mlxsw_sp_vport_vid_get(const struct mlxsw_sp_port *mlxsw_sp_vport)
 }
 
 static inline u16
-mlxsw_sp_vport_fid_get(const struct mlxsw_sp_port *mlxsw_sp_vport)
+mlxsw_sp_vport_vfid_get(const struct mlxsw_sp_port *mlxsw_sp_vport)
 {
-	return mlxsw_sp_vport->vport.f->fid;
+	return mlxsw_sp_vport->vport.vfid->vfid;
 }
 
 static inline struct mlxsw_sp_port *
@@ -298,14 +298,14 @@ mlxsw_sp_port_vport_find(const struct mlxsw_sp_port *mlxsw_sp_port, u16 vid)
 }
 
 static inline struct mlxsw_sp_port *
-mlxsw_sp_port_vport_find_by_fid(const struct mlxsw_sp_port *mlxsw_sp_port,
-				u16 fid)
+mlxsw_sp_port_vport_find_by_vfid(const struct mlxsw_sp_port *mlxsw_sp_port,
+				 u16 vfid)
 {
 	struct mlxsw_sp_port *mlxsw_sp_vport;
 
 	list_for_each_entry(mlxsw_sp_vport, &mlxsw_sp_port->vports_list,
 			    vport.list) {
-		if (mlxsw_sp_vport_fid_get(mlxsw_sp_vport) == fid)
+		if (mlxsw_sp_vport_vfid_get(mlxsw_sp_vport) == vfid)
 			return mlxsw_sp_vport;
 	}
 
