21b10ebe3e3bd0c3b36475f60ad57a30911c9aa9
juezhi6668 <1649417471@qq.com>
2026-04-30 14:36:33 +0800

    Revert "mlxsw: spectrum: Fix compilation error when CLS_ACT isn't set"
    This reverts commit b5da550be6784d1e5c7e2242196d5ce124821f7d.

--- include/net/act_api.h
+++ include/net/act_api.h
@@ -179,9 +179,6 @@ int tcf_action_copy_stats(struct sk_buff *, struct tc_action *, int);
 #define tc_for_each_action(_a, _exts) \
 	list_for_each_entry(_a, &(_exts)->actions, list)
 
-#define tc_single_action(_exts) \
-	(list_is_singular(&(_exts)->actions))
-
 static inline void tcf_action_stats_update(struct tc_action *a, u64 bytes,
 					   u64 packets, u64 lastuse)
 {
@@ -195,7 +192,6 @@ static inline void tcf_action_stats_update(struct tc_action *a, u64 bytes,
 
 #define tc_no_actions(_exts) true
 #define tc_for_each_action(_a, _exts) while ((void)(_a), 0)
-#define tc_single_action(_exts) false
 #define tcf_action_stats_update(a, bytes, packets, lastuse)
 
 #endif /* CONFIG_NET_CLS_ACT */
