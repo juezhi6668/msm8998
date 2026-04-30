a4d6e16234dce711f90f0b212672e5d241d759c2
juezhi6668 <1649417471@qq.com>
2026-04-30 14:13:58 +0800

    Revert "ipv4: Namespaceify tcp synack retries sysctl knob"
    This reverts commit 81660db29cae82b45f68eb5f9b0cfa2ef53249de.

--- include/net/netns/ipv4.h
+++ include/net/netns/ipv4.h
@@ -100,7 +100,6 @@ struct netns_ipv4 {
 	int sysctl_tcp_keepalive_intvl;
 
 	int sysctl_tcp_syn_retries;
-	int sysctl_tcp_synack_retries;
 
 	int sysctl_tcp_default_init_rwnd;
 
--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -2466,7 +2466,6 @@ static int __net_init tcp_sk_init(struct net *net)
 	net->ipv4.sysctl_tcp_keepalive_intvl = TCP_KEEPALIVE_INTVL;
 
 	net->ipv4.sysctl_tcp_syn_retries = TCP_SYN_RETRIES;
-	net->ipv4.sysctl_tcp_synack_retries = TCP_SYNACK_RETRIES;
 
 	net->ipv4.sysctl_tcp_default_init_rwnd = TCP_INIT_CWND * 2;
 
