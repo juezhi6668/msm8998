87b18f61cfc8a7b303d94050b906a76cef0601bc
juezhi6668 <1649417471@qq.com>
2026-04-30 13:46:26 +0800

    Revert "ipv4: early demux should be aware of fragments"
    This reverts commit 9475c266822a0dffba1067b50390b1d9207c4cfc.

--- net/ipv4/ip_input.c
+++ net/ipv4/ip_input.c
@@ -317,10 +317,7 @@ static int ip_rcv_finish(struct net *net, struct sock *sk, struct sk_buff *skb)
 	struct rtable *rt;
 	void (*edemux)(struct sk_buff *skb);
 
-	if (sysctl_ip_early_demux &&
-	    !skb_dst(skb) &&
-	    !skb->sk &&
-	    !ip_is_fragment(iph)) {
+	if (sysctl_ip_early_demux && !skb_dst(skb) && !skb->sk) {
 		const struct net_protocol *ipprot;
 		int protocol = iph->protocol;
 
