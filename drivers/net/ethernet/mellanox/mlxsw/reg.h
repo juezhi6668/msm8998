fbe6f4deb8f0d88130bb16a62581c8fa00082b35
juezhi6668 <1649417471@qq.com>
2026-04-30 14:53:37 +0800

    Revert "mlxsw: reg: Use correct PBMC register length"
    This reverts commit 6f0031723d4ff008a82e8946148a77daad508e43.

--- drivers/net/ethernet/mellanox/mlxsw/reg.h
+++ drivers/net/ethernet/mellanox/mlxsw/reg.h
@@ -2483,7 +2483,7 @@ static inline void mlxsw_reg_pptb_pack(char *payload, u8 local_port)
  * allocation for different Prios, and the Pause threshold management.
  */
 #define MLXSW_REG_PBMC_ID 0x500C
-#define MLXSW_REG_PBMC_LEN 0x6C
+#define MLXSW_REG_PBMC_LEN 0x68
 
 static const struct mlxsw_reg_info mlxsw_reg_pbmc = {
 	.id = MLXSW_REG_PBMC_ID,
